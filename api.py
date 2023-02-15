5ee2f70f9e2782ecc6576af3a95a42ce21993d3d
WltrH <WltrHrng@gmail.com>
2023-02-15 15:31:39 +0100

    récupération json

--- api.py
+++ api.py
@@ -14,16 +14,31 @@ def ping():
     except:
         return False
 
-
-
-
-'''#ping de l'API CoinGecko
-bping =cg.ping()
-print(bping)
-
-#récupérer le btc vs usd
-btc = cg.get_price(ids='bitcoin', vs_currencies='usd')
-print(btc)
-'''
 #print le retour de la fonction ping()
 print(ping())
+
+#récupération dans une liste des 10 premiers crypto-monnaies
+def top10():
+    top10 = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc', per_page=10, page=1, sparkline=False, price_change_percentage='1h,24h,7d')
+    #mise des information dans un fichier json dans le dossier json du projet
+    with open('json/top10.json', 'w') as f:
+        json.dump(top10, f)
+    return top10
+
+#fonction de récupération de l'intégralité des crypto-monnaies
+def all():
+    all = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc', per_page=100, page=1, sparkline=False, price_change_percentage='1h,24h,7d')
+    #mise des information dans un fichier json dans le dossier json du projet
+    with open('json/all.json', 'w') as f:
+        json.dump(all, f)
+    return all
+
+#fonction pour rafraichir les données 5 fois par minutes
+def refresh():
+    for i in range(5):
+        top10()
+        time.sleep(12)
+
+#test des fonctions
+print(top10())
+print(all())
