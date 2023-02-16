4e31e57c2b8baeaaeef85e6d78fa928b5b2f56b3
WltrH <WltrHrng@gmail.com>
2023-02-16 10:42:17 +0100

    recherche stats

--- api.py
+++ api.py
@@ -17,8 +17,18 @@ def ping():
 #print le retour de la fonction ping()
 print(ping())
 
+pinged = cg.ping()
+
+#récupérer les 5 premières crypto-monnaies en USD
+def top5usd():
+    top5 = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc', per_page=5, page=1, sparkline=False, price_change_percentage='1h,24h,7d')
+    #mise des information dans un fichier json dans le dossier json du projet
+    with open('json/top5.json', 'w') as f:
+        json.dump(top5, f)
+    return top5
+
 #récupération dans une liste des 10 premiers crypto-monnaies en USD
-def top10():
+def top10usd():
     top10 = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc', per_page=10, page=1, sparkline=False, price_change_percentage='1h,24h,7d')
     #mise des information dans un fichier json dans le dossier json du projet
     with open('json/top10.json', 'w') as f:
@@ -26,16 +36,16 @@ def top10():
     return top10
 
 
-'''#fonction Top10 avec le paramètre de la currency
-def top10(currency):
+#fonction Top10 avec le paramètre de la currency
+def top10cur(currency):
     top10 = cg.get_coins_markets(vs_currency=currency, order='market_cap_desc', per_page=10, page=1, sparkline=False, price_change_percentage='1h,24h,7d')
     #mise des information dans un fichier json dans le dossier json du projet
     with open('json/top10.json', 'w') as f:
         json.dump(top10, f)
-    return top10'''
+    return top10
 
 #fonction de récupération de l'intégralité des crypto-monnaies
-def all():
+def allusd():
     all = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc', per_page=100, page=1, sparkline=False, price_change_percentage='1h,24h,7d')
     #mise des information dans un fichier json dans le dossier json du projet
     with open('json/all.json', 'w') as f:
@@ -43,24 +53,28 @@ def all():
     return all
 
 
-'''#fonction de récupération de l'intégralité des crypto-monnaies en choisissant la currency
-def all(currency):
+#fonction de récupération de l'intégralité des crypto-monnaies en choisissant la currency
+def allcur(currency):
     all = cg.get_coins_markets(vs_currency=currency, order='market_cap_desc', per_page=100, page=1, sparkline=False, price_change_percentage='1h,24h,7d')
     #mise des information dans un fichier json dans le dossier json du projet
     with open('json/all.json', 'w') as f:
         json.dump(all, f)
-    return all'''
+    return all
 
 
 
-'''#fonction pour rafraichir les données 5 fois par minutes
+#fonction pour rafraichir les données 5 fois par minutes
 def refresh():
     for i in range(5):
         top10()
         all()
-        time.sleep(12)'''
+        time.sleep(12)
 
 
 #test des fonctions
-print (top10())
-print (all())
+print (ping())
+#print (top10usd())
+#print (top10cur('eur'))
+#print (allusd())
+#print (allcur('eur'))
+print(top5usd())
