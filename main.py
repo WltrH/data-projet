66bcfd2676eedc8986703059a96dd3909fcc3e6b
WltrH <WltrHrng@gmail.com>
2023-02-23 17:05:10 +0100

    extra graph

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import api as api
-from pycoingecko import CoinGeckoAPI
-cg = CoinGeckoAPI()
 import datetime
 import warnings
 warnings.filterwarnings('ignore')
+
+from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
+from streamlit_extras import dataframe_explorer
+from streamlit_extras.chart_container import chart_container
+
+
 
 ####################Setting######################
 page_title = "Crypto-Monnaies Data"
@@ -17,9 +21,7 @@ layout = "centered"
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
 
-
-
-#################### SIDEBAR ####################
+################### SIDEBAR ####################
 # variable de date du jour
 date_jour = datetime.date.today()
 date_jour_ = date_jour - datetime.timedelta(days=30)
@@ -44,6 +46,11 @@ start_date = st.sidebar.date_input('Choisir la date de début', value=date_jour_
 # placement d'une case pour choisir la date de fin
 end_date = st.sidebar.date_input('Choisir la date de fin', value=date_jour)
 
+# si les dates start_date et end_date ont un différenciel de plus de 30 jours alors on ajuste les dates
+if (end_date - start_date).days > 30:
+    # on ajuste la date de début à la date de fin - 30 jours
+    start_date = end_date - datetime.timedelta(days=30)
+
 if st.sidebar.button('Mettre à jour les données'):
     # mise à jour des données
     #api.refresh2(currency, id1, id2, start_date, end_date)
@@ -62,22 +69,20 @@ st.sidebar.subheader("Powered by :")
 # insertion d'une image dans la sidebar
 st.sidebar.image('img/logo.png', width=200)
 
-# lancement des fonctions
+
+################### MAIN ####################
 
 
-
-#################### PAGE 1 ####################
-
-# création du titre
-st.title("Crypto-Monnaies Data")
-
-# création du sous-titre
+################### TOP 10 ####################
 st.subheader("Top 10 des crypto-monnaies")
 
 # création d'un tableau des 10 premières crypto monnaies, en partant du fichier json Top10
 # avec les colonnes suivantes : nom, prix, volume, capitalisation, %1h, %24h, %7j
 df = pd.read_json('json/top10.json')
 df = df[['name', 'current_price', 'total_volume', 'market_cap', 'price_change_percentage_1h_in_currency', 'price_change_percentage_24h_in_currency', 'price_change_percentage_7d_in_currency']]
+
+# renommer les colonnes
+#df = df.rename(columns={'name': 'Nom', 'current_price': 'Prix', 'total_volume': 'Volume', 'market_cap': 'Capitalisation', 'price_change_percentage_1h_in_currency': '%1h', 'price_change_percentage_24h_in_currency': '%24h', 'price_change_percentage_7d_in_currency': '%7j'})
 
 # mise en forme des données en % pour les colonnes price_change_percentage_1h_in_currency, price_change_percentage_24h_in_currency, price_change_percentage_7d_in_currency
 df['price_change_percentage_1h_in_currency'] = df['price_change_percentage_1h_in_currency'].apply(lambda x: str(round(x, 2)) + '%')
@@ -91,7 +96,6 @@ df['market_cap'] = df['market_cap'].apply(lambda x: str(round(x, 2)) + ' €')
 # renommage des colonnes
 df = df.rename(columns={'name': 'Nom', 'current_price': 'Prix', 'total_volume': 'Volume', 'market_cap': 'Capitalisation', 'price_change_percentage_1h_in_currency': '%1h', 'price_change_percentage_24h_in_currency': '%24h', 'price_change_percentage_7d_in_currency': '%7j'})
 st.table(df)
-
 # separation des graphiques
 st.markdown('---')
 
@@ -125,16 +129,12 @@ ax.set_title('Volume des 10 premières crypto monnaies')
 
 st.pyplot(fig)
 
-
-
-st.markdown('---')
-st.subheader("travail en cours ici")
-#ouverture du fichier json avec en paramètre l'id et la devise
-# afficher l'id et la devise
-st.write(id1)
-st.write(currency)
-
-df = pd.read_json('json/''histo-'+id1+'-'+currency+'.json')
+################### HISTO ####################
+# test des graphiques
+st.subheader("Historique des prix")
+# exploration des dataframes avec streamlit_extras
+#récupération des données dans un dataframe
+df = pd.read_json('json/histo-bitcoin-eur.json')
 
 #création d'un dataframe avec les données de la colonne prices, market_caps et total_volumes avec séparation de la date
 df[['date', 'price']] = df['prices'].apply(lambda x: pd.Series([x[0], x[1]])).add_prefix('price_')
@@ -144,52 +144,53 @@ df[['date', 'total_volume']] = df['total_volumes'].apply(lambda x: pd.Series([x[
 #suppression des colonnes inutiles
 df.drop(['prices', 'market_caps', 'total_volumes'], axis=1, inplace=True)
 
-#conversion de la date en datetime
+#mise aua format datetime de la colonne date
 df['date'] = pd.to_datetime(df['date'], unit='ms')
-#mise de la date au format datetime année-mois-jour-heure
-df['date'] = df['date'].dt.strftime('%Y-%m-%d %H:%M')
+
+#retirer les heures de la date
+df['date'] = df['date'].dt.strftime('%Y-%m-%d')
+
+#mise de la date en index
+#df.set_index('date', inplace=True)
+
+
+#mise au format monnaie de la colonne price
+df['price'] = df['price'].apply(lambda x: '{:,.2f}'.format(x))
+#mise au format monnaie de la colonne market_cap
+df['market_cap'] = df['market_cap'].apply(lambda x: '{:,.2f}'.format(x))
+#mise au format monnaie de la colonne total_volume
+df['total_volume'] = df['total_volume'].apply(lambda x: '{:,.2f}'.format(x))
+
+# mise du dataframe au format euro
+df['price'] = df['price'].apply(lambda x: x + ' €')
+df['market_cap'] = df['market_cap'].apply(lambda x: x + ' €')
+df['total_volume'] = df['total_volume'].apply(lambda x: x + ' €')
+
+st.table(df.iloc[0:10])
+# separation des graphiques
+st.markdown('---')
+
+df = pd.read_json('json/histo-bitcoin-eur.json')
+
+#création d'un dataframe avec les données de la colonne prices, market_caps et total_volumes avec séparation de la date
+df[['date', 'price']] = df['prices'].apply(lambda x: pd.Series([x[0], x[1]])).add_prefix('price_')
+df[['date', 'market_cap']] = df['market_caps'].apply(lambda x: pd.Series([x[0], x[1]])).add_prefix('market_cap_')
+df[['date', 'total_volume']] = df['total_volumes'].apply(lambda x: pd.Series([x[0], x[1]])).add_prefix('total_volume_')
+
+#suppression des colonnes inutiles
+df.drop(['prices', 'market_caps', 'total_volumes'], axis=1, inplace=True)
+
+#mise aua format datetime de la colonne date
+df['date'] = pd.to_datetime(df['date'], unit='ms')
+
+#retirer les heures de la date
+df['date'] = df['date'].dt.strftime('%Y-%m-%d')
+
 #mise de la date en index
 df.set_index('date', inplace=True)
 
-# mise en forme des données en € pour les colonnes price, market_cap, total_volume
-df['price'] = df['price'].apply(lambda x: str(round(x, 2)) + ' €')
-df['market_cap'] = df['market_cap'].apply(lambda x: str(round(x, 2)) + ' €')
-df['total_volume'] = df['total_volume'].apply(lambda x: str(round(x, 2)) + ' €')
 
-
-#enlever les heurs de la date
-df.index = df.index.str[:10]
-
-
-#affichage les 10 premières lignes du dataframe
-st.table(df.iloc[0:10])
-#st.pyplot(df)
-#graphipe de la colonne price
-df['price'].plot(figsize=(15, 5), title='Price')
-st.plotly_chart(df)
-
-#afficher le graphique en plotly
-
-
-'''# titre du graphique
-st.subheader("historique de l'évolution du prix")
-# graphique de l'évolution du prix de la crypto monnaie
-df = pd.read_json('json/history.json')
-# mise en forme des données de la colonne date
-df['date'] = df['date'].apply(lambda x: datetime.fromtimestamp(x/1000).strftime('%d/%m/%Y %H:%M:%S'))
-#mise en forme des données en € pour la colonne price
-df['price'] = df['price'].apply(lambda x: str(round(x, 6)) + ' €')
-#indes de la date
-df['date'] = pd.to_datetime(df['date'])
-#graphique
-fig, ax = plt.subplots(figsize=(10, 5))
-ax.plot(df['date'], df['price'])
-ax.set_xlabel('Date')
-ax.set_ylabel('Prix')
-ax.set_title('Evolution du prix de la crypto monnaie')
-st.pyplot(fig)
-st.markdown('---')
-
-'''
-
+with chart_container(df):
+    st.write("## Chart")
+    st.area_chart(df[["price"]])
 
